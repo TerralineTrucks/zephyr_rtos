cd3a235e1aee25294d830af8b754dae8d93c4ebe
Matthew Christian <matthew.christian@terralinetrucks.com>
2024-12-03 11:06:15 -0700

    Include direct-mapped qspi regions in mpu region restrictions

--- soc/nxp/s32/s32ze/mpu_regions.c
+++ soc/nxp/s32/s32ze/mpu_regions.c
@@ -10,26 +10,32 @@
 #define DEVICE_REGION_START 0x40000000UL
 #define DEVICE_REGION_END   0x76FFFFFFUL
 
+#ifdef CONFIG_MEMC_NXP_S32_QSPI
+#define QSPI0_AHB_REGION_START 0x00000000UL
+#define QSPI0_AHB_REGION_END   0x07FFFFFFUL
+#define QSPI1_AHB_REGION_START 0x10000000UL
+#define QSPI1_AHB_REGION_END   0x17FFFFFFUL
+#endif // CONFIG_MEMC_NXP_S32_QSPI
+
 static const struct arm_mpu_region mpu_regions[] = {
-	MPU_REGION_ENTRY("vector",
-			 (uintptr_t)_vector_start,
+	MPU_REGION_ENTRY("vector", (uintptr_t)_vector_start,
 			 REGION_RAM_TEXT_ATTR((uintptr_t)_vector_end)),
 
-	MPU_REGION_ENTRY("SRAM_TEXT",
-			 (uintptr_t)__text_region_start,
+	MPU_REGION_ENTRY("SRAM_TEXT", (uintptr_t)__text_region_start,
 			 REGION_RAM_TEXT_ATTR((uintptr_t)__rodata_region_start)),
 
-	MPU_REGION_ENTRY("SRAM_RODATA",
-			 (uintptr_t)__rodata_region_start,
+	MPU_REGION_ENTRY("SRAM_RODATA", (uintptr_t)__rodata_region_start,
 			 REGION_RAM_RO_ATTR((uintptr_t)__rodata_region_end)),
 
-	MPU_REGION_ENTRY("SRAM_DATA",
-			 (uintptr_t)_image_ram_start,
+	MPU_REGION_ENTRY("SRAM_DATA", (uintptr_t)_image_ram_start,
 			 REGION_RAM_ATTR((uintptr_t)__kernel_ram_end)),
 
-	MPU_REGION_ENTRY("DEVICE",
-			 DEVICE_REGION_START,
-			 REGION_DEVICE_ATTR(DEVICE_REGION_END)),
+	MPU_REGION_ENTRY("DEVICE", DEVICE_REGION_START, REGION_DEVICE_ATTR(DEVICE_REGION_END)),
+
+#ifdef CONFIG_MEMC_NXP_S32_QSPI
+	MPU_REGION_ENTRY("QSPI", (uintptr_t)QSPI0_AHB_REGION_START,
+			 REGION_FLASH_ATTR(QSPI1_AHB_REGION_END)),
+#endif // CONFIG_MEMC_NXP_S32_QSPI
 };
 
 const struct arm_mpu_config mpu_config = {
