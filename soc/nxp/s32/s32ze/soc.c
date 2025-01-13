0a0714346d97de82154d2276cc89d879d61587a3
mcc-terraline <matthew.christian@terralinetrucks.com>
2025-01-13 14:19:19 -0700

    Add working S32ze SoC SRAM/TCM for the pre-kernel init
    Add working S32ze SoC SRAM/TCM for the pre-kernel init

--- soc/nxp/s32/s32ze/soc.c
+++ soc/nxp/s32/s32ze/soc.c
@@ -35,9 +35,12 @@ void ALWAYS_INLINE soc_partition_sram_init(uint32_t base_addr, uint32_t sz)
 {
 	// Clear previous init status, if any
 	REG_WRITE32(base_addr + 0xc, 0x00000001);
+
 	// Set initialization address register start & end
 	REG_WRITE32(base_addr + 0x4, 0x00000000);
-	REG_WRITE32(base_addr + 0x8, sz - 1);
+	// If using local addresses, it is 8-byte shifted, but should be the last one to program
+	REG_WRITE32(base_addr + 0x8, (sz >> 3) - 1);
+
 	// Trigger the initialization: set INITREQ in PRAMCR, and wait for completion
 	REG_WRITE32(base_addr + 0x0, 0x00000001);
 	REG_CHECK_MASK(base_addr + 0xc, 0x00000001, 0x00000001);
@@ -45,6 +48,85 @@ void ALWAYS_INLINE soc_partition_sram_init(uint32_t base_addr, uint32_t sz)
 
 void z_arm_platform_init(void)
 {
+#ifdef CONFIG_MCUBOOT
+/* For booting, this should be configured to use stack located in
+ * Rtu0 Core0 TCMs. Configure the IMP_*TCMREGIONR for the core to use them.
+ * TCM A = 64 KB, has 1 wait state, 0x30000000
+ * TCM B = 16 KB, has 0 wait state, 0x30100000
+ * TCM C = 16 KB, has 1 wait state, 0x30200000
+ */
+#define RTU0_CORE0_TCMBASE  0x30000000
+#define TCMA_OFFSET         0x0
+#define TCMB_OFFSET         0x100000
+#define TCMC_OFFSET         0x200000
+#define TCMREGION_SIZE_16KB 0b00101
+#define TCMREGION_SIZE_64KB 0b00111
+
+#define TCM_INIT_BASE CONFIG_SRAM_BASE_ADDRESS
+#define TCM_OP_COUNT  (CONFIG_SRAM_SIZE * 1024 / 8)
+
+	/* Configure TCM A */
+	uint32_t val = RTU0_CORE0_TCMBASE + TCMA_OFFSET;
+	val |= TCMREGION_SIZE_64KB << 2;
+	val |= 1 << 8; // Wait States (1/0)
+	val |= 1 << 1; // Enable (1), Disable (0) for EL2
+	val |= 1 << 0; // Enable (1), Disable (0) for EL1 & EL0
+	__set_CP(15, 0, val, 9, 1, 0);
+
+	/* Configure TCM B */
+	val = RTU0_CORE0_TCMBASE + TCMB_OFFSET;
+	val |= TCMREGION_SIZE_16KB << 2;
+	val |= 0 << 8; // Wait States (1/0)
+	val |= 1 << 1; // Enable (1), Disable (0) for EL2
+	val |= 1 << 0; // Enable (1), Disable (0) for EL1 & EL0
+	__set_CP(15, 0, val, 9, 1, 1);
+
+	/* Configure TCM C */
+	val = RTU0_CORE0_TCMBASE + TCMC_OFFSET;
+	val |= TCMREGION_SIZE_16KB << 2;
+	val |= 1 << 8; // Wait States (1/0)
+	val |= 1 << 1; // Enable (1), Disable (0) for EL2
+	val |= 1 << 0; // Enable (1), Disable (0) for EL1 & EL0
+	__set_CP(15, 0, val, 9, 1, 2);
+
+	/* This is intrinsically going to use stack which may not have
+	 * been initialized yet depending on hardware configuration.
+	 * Disable RAM ECC so the partial line loads for stack values to complete
+	 * the full line memory writes won't trap on access.
+	 */
+	uint32_t flash_prot_en = 1;
+	uint32_t ram_prot_en = 0;
+	__set_CP(15, 1, ((flash_prot_en << 1) | (ram_prot_en << 0)), 9, 1, 2);
+
+	/* Zero out the TCMA.
+	 * If the SRAM_BASE was moved outside of the core-local memories then this will
+	 * trap & abort on random SRAM partial loads with ECC failures (from non-initialized
+	 * system SRAM).
+	 *
+	 * Do this in QuadWord store operations to ensure the complete ECC line
+	 * fills at the same time to avoid partial RMW and faults at the bus.
+	 */
+	__asm__ volatile("qword_copy_init:			\t\n"
+			 "	mov r1, %0			\t\n"
+			 "	mov r3, %1			\t\n"
+			 "	mov r4, #0			\t\n"
+			 "	mov r5, #0			\t\n"
+			 "qword_copy:			\t\n"
+			 "	strd r4, r5, [r1]	\t\n"
+			 "  ldrd r4, r5, [r1]	\t\n"
+			 "  add r1, r1, #8		\t\n"
+			 "	sub r3, r3, #1		\t\n"
+			 "  cmp r3, #0			\t\n"
+			 "	bne qword_copy		\t\n"
+			 :
+			 : "I"(TCM_INIT_BASE), "I"(TCM_OP_COUNT)
+			 : "memory");
+
+	/* Re-enable the TCM & Cache ECC protection now. */
+	ram_prot_en = 1;
+	__set_CP(15, 1, ((flash_prot_en << 1) | (ram_prot_en << 0)), 9, 1, 2);
+#endif /* CONFIG_MCUBOOT */
+
 	/* enable peripheral port access at EL1 and EL0 */
 	__asm__ volatile("mrc p15, 0, r0, c15, c0, 0\n");
 	__asm__ volatile("orr r0, #1\n");
@@ -73,29 +155,6 @@ void z_arm_platform_init(void)
 			barrier_dsync_fence_full();
 		}
 	}
-
-	/* Need to initialize the Data SRAM for RTU0 which isn't done by the HSE Boot Rom
-	 * for some reason.
-	 */
-	// soc_partition_sram_init(0x761D0000, 0x40000);
-	// soc_partition_sram_init(0x761E0000, 0x40000);
-	// soc_partition_sram_init(0x761F0000, 0x800000);
-
-	/* Initialize the stacks. It can't use the INIT_STACKS feature since that uses 0xAA and the
-	 * compiler assumes 0 in uninit stack space for optimization -- I think.
-	 * Access to an uninitialized SRAM address will ABORT in the R52. */
-	extern char z_interrupt_stacks[];
-	extern char z_arm_fiq_stack[];
-	extern char z_arm_abort_stack[];
-	extern char z_arm_undef_stack[];
-	extern char z_arm_svc_stack[];
-	extern char z_arm_sys_stack[];
-	memset(z_arm_fiq_stack, 0, CONFIG_ARMV7_FIQ_STACK_SIZE);
-	memset(z_interrupt_stacks, 0, CONFIG_ISR_STACK_SIZE);
-	memset(z_arm_abort_stack, 0, CONFIG_ARMV7_EXCEPTION_STACK_SIZE);
-	memset(z_arm_undef_stack, 0, CONFIG_ARMV7_EXCEPTION_STACK_SIZE);
-	memset(z_arm_svc_stack, 0, CONFIG_ARMV7_SVC_STACK_SIZE);
-	memset(z_arm_sys_stack, 0, CONFIG_ARMV7_SYS_STACK_SIZE);
 }
 
 static int soc_init(void)
@@ -105,21 +164,14 @@ static int soc_init(void)
 	return 0;
 }
 
-// #ifdef CONFIG_CLOCK_CONTROL_NXP_S32
+#ifdef CONFIG_MCUBOOT
 static int soc_platform_init(void)
 {
-	// TODO: This debugging enabler is likely not necessary
-	*(volatile uint32_t *)0x4dc100c0 = 0x3cf3cf00;
-
+#ifdef CONFIG_CLOCK_CONTROL_NXP_S32
 	/* If this element is responsible for the clock configuration then the
 	 * reset must include the RTU1 partition, otherwise multiple clock element
 	 * configurations will fail.
 	 */
-
-	extern char _nocache_ram_start[];
-	extern char _nocache_ram_end[];
-	memset(&_nocache_ram_start, 0,
-	       (uintptr_t)&_nocache_ram_end - (uintptr_t)&_nocache_ram_start);
 
 	// Enable partition clocks and confirm
 	REG_SET_MASK(0x41900500, 0x00000001);
@@ -157,11 +209,32 @@ static int soc_platform_init(void)
 	REG_WRITE32(0x75500000, 0x2);
 	REG_WRITE32(0x75600000, 0x2);
 	REG_WRITE32(0x75700000, 0x2);
+#endif /* CONFIG_CLOCK_CONTROL_NXP_S32 */
 
+	/* Perform ECC Initialization on the RTU 0 Data SRAM */
+	soc_partition_sram_init(0x761D0000, 0x40000);
+	soc_partition_sram_init(0x761E0000, 0x40000);
+	soc_partition_sram_init(0x761F0000, 0x80000);
+
+#if 0 // TODO: Debug and configure this for RTU1 use.
+	/* Perform ECC Initialization on the RTU 1 Data SRAM */
+	soc_partition_sram_init(0x769D0000, 0x40000);
+	soc_partition_sram_init(0x769E0000, 0x40000);
+	soc_partition_sram_init(0x769F0000, 0x80000);
+
+	/* Perform ECC Initialization on the RTU 1 Code SRAM */
+	soc_partition_sram_init(0x768C0000, 0x100000);
+	soc_partition_sram_init(0x768D0000, 0x100000);
+	soc_partition_sram_init(0x768E0000, 0x100000);
+	soc_partition_sram_init(0x768F0000, 0x100000);
+	soc_partition_sram_init(0x76A40000, 0x100000);
+	soc_partition_sram_init(0x76A50000, 0x100000);
+	soc_partition_sram_init(0x76A60000, 0x100000);
+#endif
 	return 0;
 }
 
 SYS_INIT(soc_platform_init, EARLY, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
-// #endif // CONFIG_CLOCK_CONTROL_NXP_S32
+#endif /* CONFIG_MCUBOOT */
 
 SYS_INIT(soc_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
